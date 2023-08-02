b4d80a3cb12a6e7c2eaf94fc3a70cb6292899d44
FiggersRM <MitchFiggers@gmail.com>
2023-08-01 23:07:36 -0400

    Minor tweaks to contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -41,6 +41,9 @@ function Contact() {
         setErrorMessage("Invalid Email Address");
         return;
       }
+      else {
+        setErrorMessage('')
+      }
     } else if (inputType === "message") {
       setMessage(inputValue);
     } else {
