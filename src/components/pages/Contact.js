ea83aa5f3291807ec24a23cf1affabc4ed3956a5
FiggersRM <MitchFiggers@gmail.com>
2023-08-01 21:02:02 -0400

    Finished contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,29 +1,113 @@
-import React from "react";
-import { useForm, ValidationError } from "@formspree/react";
+import React, { useState } from "react";
+import { useForm } from "@formspree/react";
 
 function Contact() {
-  //  USE FORMSPREE
   const [state, handleSubmit] = useForm("xqkveppl");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
+
   if (state.succeeded) {
     return <p>Thanks for contacting me!</p>;
   }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !name || !message) {
+      setErrorMessage("All fields required");
+      return;
+    }
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+      setErrorMessage("Invalid Email Address");
+      return;
+    }
+    alert("Thank you for contacting me!");
+
+    setEmail("");
+    setName("");
+    setMessage("");
+    setErrorMessage("");
+  };
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "message") {
+      setMessage(inputValue);
+    } else {
+      setName(inputValue);
+    }
+  };
+
   return (
-    <div>
+    <div className="ContactForm">
       {" "}
       <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email Address</label>
-        <input id="email" type="email" name="email" />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
-        <textarea id="message" name="message" />
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
+        <div className="inputdiv">
+          <label htmlFor="name">
+            {name.length ? (
+              <span style={{ color: "red" }}></span>
+            ) : (
+              <span style={{ color: "red" }}>*</span>
+            )}
+            Your Name:{" "}
+          </label>
+          <input
+            className="emailinput"
+            type="name"
+            name="name"
+            placeholder="Your Name"
+            value={name}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="inputdiv">
+          <label htmlFor="email">
+            {email.length ? (
+              <span style={{ color: "red" }}></span>
+            ) : (
+              <span style={{ color: "red" }}>*</span>
+            )}
+            Your Email Address:{" "}
+          </label>
+          <input
+            className="emailinput"
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        {message.length ? (
+              <span style={{ color: "red" }}></span>
+            ) : (
+              <span style={{ color: "red" }}>*</span>
+            )}
+        <textarea
+          id="message"
+          name="message"
+          value={message}
+          class="contactmessage"
+          placeholder="message"
+          onChange={handleInputChange}
         />
-        <button type="submit" disabled={state.submitting}>
+        <button type="submit" className="submitBtn" onClick={handleFormSubmit}>
           Submit
         </button>
       </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text" style={{color: 'red'}}>{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
