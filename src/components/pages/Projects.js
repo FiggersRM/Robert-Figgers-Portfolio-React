2f3ec534631bd23848b3fe263b1cd649ade34d07
FiggersRM <MitchFiggers@gmail.com>
2023-08-01 23:26:33 -0400

    Added gh icons to projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,6 +5,7 @@ import EC from "../../images/ECommerce.png";
 import WF from "../../images/WF.png";
 import SN from "../../images/SN.jpg";
 import EI from "../../images/EatIt.png";
+import {FaGithub} from 'react-icons/fa6';
 
 function Projects() {
   return (
@@ -16,7 +17,7 @@ function Projects() {
                 alt="This is a logo for the website What the Hell's for Dinner. It features 3 small circles slightly overlapping each other in a horizontal direction. The first one is red with an outline of a knife. The middle one is light green with an outline of a spoon, and the right one is yellow-orange with an outline of a fork."
               />
               </a>
-              <a  class="linkboxes" href="https://figgersrm.github.io/WTHFD/">What the Hell's for Dinner? <a href="https://github.com/FiggersRM/WTHFD">| GitHub</a></a>
+              <a  class="linkboxes" href="https://figgersrm.github.io/WTHFD/">What the Hell's for Dinner?</a><a href="https://github.com/FiggersRM/WTHFD" className="ghIcon"> <FaGithub /></a>
     </div>
     <div className="card">
     <a href="https://limitless-badlands-70129.herokuapp.com/">
@@ -26,7 +27,7 @@ function Projects() {
                 style={{width: 175, paddingTop: 30}}
               />
               </a>
-              <a  class="linkboxes" href="https://limitless-badlands-70129.herokuapp.com/">Roam Ready <a href="https://github.com/FiggersRM/Roam-Ready">| GitHub</a></a>
+              <a  class="linkboxes" href="https://limitless-badlands-70129.herokuapp.com/">Roam Ready </a><a href="https://github.com/FiggersRM/Roam-Ready" className="ghIcon"> <FaGithub /></a>
     </div>
     <div className="card">
     <a href="https://drive.google.com/file/d/1bsA68kYsIEaknKSGiEAXmy7p6yHFG4O-/view?usp=drive_link">
@@ -36,7 +37,7 @@ function Projects() {
                 style={{borderRadius: '5px'}}
               />
               </a>
-              <a  class="linkboxes" href="https://drive.google.com/file/d/1bsA68kYsIEaknKSGiEAXmy7p6yHFG4O-/view?usp=drive_link">E-Commerce Server <a href="https://github.com/FiggersRM/E-Commerce-Back-End">| GitHub</a></a>
+              <a  class="linkboxes" href="https://drive.google.com/file/d/1bsA68kYsIEaknKSGiEAXmy7p6yHFG4O-/view?usp=drive_link">E-Commerce Serve</a><a href="https://github.com/FiggersRM/E-Commerce-Back-End" className="ghIcon"><FaGithub /></a>
     </div>
     <div className="card">
     <a href="https://figgersrm.github.io/5-Day-Weather-Forecast-C6/">
@@ -45,7 +46,7 @@ function Projects() {
                 alt="A screenshot of the working application that is showing a five day forecast for the city of Richmond Virginia"
               />
               </a>
-              <a  class="linkboxes" href="https://figgersrm.github.io/5-Day-Weather-Forecast-C6/">5-Day Weather Forecaster <a href="https://github.com/FiggersRM/5-Day-Weather-Forecast-C6">| GitHub</a></a>
+              <a  class="linkboxes" href="https://figgersrm.github.io/5-Day-Weather-Forecast-C6/">5-Day Weather Forecaster</a><a href="https://github.com/FiggersRM/5-Day-Weather-Forecast-C6" className="ghIcon"> <FaGithub /></a>
     </div>
     <div className="card">
     <a href="https://drive.google.com/file/d/1VckhwUFNlwP1ngZ7dBG7F6JJBe5k5Dfw/view?usp=drive_link">
@@ -55,7 +56,7 @@ function Projects() {
                 style={{paddingTop: 30}}
               />
               </a>
-              <a  class="linkboxes" href="https://drive.google.com/file/d/1VckhwUFNlwP1ngZ7dBG7F6JJBe5k5Dfw/view?usp=drive_link">Social Network API <a href="https://github.com/FiggersRM/Social-Network-API">| GitHub</a></a>
+              <a  class="linkboxes" href="https://drive.google.com/file/d/1VckhwUFNlwP1ngZ7dBG7F6JJBe5k5Dfw/view?usp=drive_link">Social Network API</a><a href="https://github.com/FiggersRM/Social-Network-API" className="ghIcon"> <FaGithub /></a>
     </div>
     <div className="card">
     <a href="https://github.com/FiggersRM/Eat-It">
@@ -65,7 +66,7 @@ function Projects() {
                 style={{width: 205, paddingTop: 40}}
               />
               </a>
-              <a  class="linkboxes" href="https://github.com/FiggersRM/Eat-It">Eat It <a href="https://github.com/FiggersRM/Eat-It">| GitHub</a></a>
+              <a  class="linkboxes" href="https://github.com/FiggersRM/Eat-It">Eat It</a><a href="https://github.com/FiggersRM/Eat-It" className="ghIcon"> <FaGithub /></a>
     </div>
   </>
     );
